efe2f2cbacc1a7bf74f1b4ea32be4baff9ebc3ee
UreshiiPanda <jojohoughton22@gmail.com>
2024-03-21 13:55:19 -0400

    navbar initiated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '480px',   // Small screens, mobile phones
+      'md': '768px',   // Medium screens, tablets
+      'lg': '1024px',  // Large screens, laptops/desktops
+      'xl': '1280px',  // Extra large screens, larger desktops
+      '2xl': '1536px', // Extra large screens, 2x larger desktops
+    }
   },
   plugins: [],
 }
